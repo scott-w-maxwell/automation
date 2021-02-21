4b66f1e53a5692f12e743d9b3e635c0e2b594218
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2021-02-20 21:03:00 -0700

    Reverted function back.

--- xml_translate.py
+++ xml_translate.py
@@ -3,13 +3,22 @@ from bs4 import BeautifulSoup as soup
 from googletrans import Translator #This needs to be version: 4.0.0-rc.1
 import html
 import bs4
-import re
+
+lang = input("Language: ").lower()
+file_name = input("File Path: ")
+
+# Open the XML file as an ElementTree
+tree=ET.parse(f'{os}Tree.xml')
+root = tree.getroot()
+
+# Convert ElementTree to a string
+xmlstr = ET.tostring(root, encoding='unicode', method='xml')
+xmlstr = html.unescape(xmlstr)
 
 
-lang = input("Language to translate to: ").lower()
-file_name = input('File Name: ')
+def translate(text):
 
-def translate(text lang):
+    global lang
 
     # Use GTranslate and compare with list of translations
     translator = Translator(service_urls=['translate.google.com'])
@@ -17,24 +26,16 @@ def translate(text lang):
 
     return translated_text
 
+def parse_root(x):
 
-def parse_root(file_name, lang):
+    global xmlstr
 
-	# Open the XML file as an ElementTree
-	tree=ET.parse(f'{file_name}')
-	root = tree.getroot()
-
-	# Convert ElementTree to string
-	xmlstr = ET.tostring(root, encoding='unicode', method='xml')
-	xmlstr = html.unescape(xmlstr)
-
-    for child in tree:
+    for child in x:
 
         split_text = child.text.split('\n')
 
         for elem in split_text:
 
-        	# Ensuring not to translate links. (also avoids bs4 warning)
             if 'http' not in elem:
                 elem_text = soup(elem, 'html.parser').text
 
@@ -43,14 +44,17 @@ def parse_root(file_name, lang):
 
                 # ensure words are in string
                 if any(a.isalpha() for a in elem_text):
-                    translation = translate(elem_text, lang)
+                    translation = translate(elem_text)
                     if translation:
                         xmlstr = xmlstr.replace(elem_text, translation)
                         
-        # Call function on children
+        # Call function recursively
         parse_root(child)
 
-	# Save XML string as XML file
-	with open(f"/Finished/{file_name}_translated.xml", "wb") as f:
-		f.write(xmlstr.encode('utf-8'))
 
+# parse ElementTree root
+parse_root(root)
+
+# Save XML string as XML file
+with open(f"{lang}_translated_to_{lang}.xml", "wb") as f:
+	f.write(xmlstr.encode('utf-8'))
